9f76298cece55fddf881f589bc0276983340c13c
ComplexSleep <135557060+ComplexSleep@users.noreply.github.com>
2026-01-22 15:07:48 +0800

    loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+-- VOID HUB CRACKED | PUBLIC LOADER
+
+print("🌌 Void Hub Cracked Loader starting...")
+
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/main.lua"
+))()
